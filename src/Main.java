3086e6af35e6acd82d2868597bfccd02292bebe4
Neto test <foxekaterina@yandex.ru>
2022-12-04 14:24:48 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int cost = 5879;
+        int oneBonusMile = 20;
+        int quantity = cost / oneBonusMile;
+
+        System.out.println("Начисленные мили " + quantity);
+
+    }
+}
